1b9dbcafdbee74e2266151fe5cadaefd01e0578c
Aaron Rodriguez <aatrodri@ucsc.edu>
2023-10-27 13:06:38 -0700

    Commit

--- src/main.js
+++ src/main.js
@@ -1,3 +1,10 @@
 //Aaron Rodriguez
 
-let game = new Phaser.Game();
+let config = {
+    type: Phaser.CANVAS,
+    width: 640,
+    height: 480,
+    scene: [ Menu, Play ]
+  }
+  
+  let game = new Phaser.Game(config);
