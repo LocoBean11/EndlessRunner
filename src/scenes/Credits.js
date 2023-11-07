af9244c6cc3a3d39a4508b8e8b5ea99fe2ef5221
Aaron Rodriguez <aatrodri@ucsc.edu>
2023-11-06 23:14:29 -0800

    Changes

--- src/scenes/Credits.js
+++ src/scenes/Credits.js
@@ -10,6 +10,39 @@ preload(){
 create() {
     this.add.image(0, 0, 'credits').setOrigin(0, 0);
     this.hurtSoundEffect = this.sound.add('hitHurt');
+
+    const creditsText = this.add.text(320, 75, "CREDITS")
+    creditsText.fontFamily = 'Times New Roman';
+    creditsText.setFontSize(40); 
+    creditsText.setOrigin(0.5, 0.5); // Center the text
+
+    const credits2Text = this.add.text(320, 150, "Art and Programming: Aaron Rodriguez")
+    credits2Text.fontFamily = 'Times New Roman';
+    credits2Text.setFontSize(20); 
+    credits2Text.setOrigin(0.5, 0.5); // Center the text
+
+
+    const credits3Text = this.add.text(320, 200, "Game Over Jingle: Aaron Rodriguez")
+    credits3Text.fontFamily = 'Times New Roman';
+    credits3Text.setFontSize(20); 
+    credits3Text.setOrigin(0.5, 0.5); // Center the text
+
+    const credits4Text = this.add.text(320, 350, "")
+    credits4Text.fontFamily = 'Times New Roman';
+    credits4Text.setFontSize(20); 
+    credits4Text.setOrigin(0.5, 0.5); // Center the text
+
+    const credits5Text = this.add.text(320, 300, "")
+    credits5Text.fontFamily = 'Times New Roman';
+    credits5Text.setFontSize(20); 
+    credits5Text.setOrigin(0.5, 0.5); // Center the text
+
+    const credits6Text = this.add.text(515, 470, "Hit DOWN arrow key to go back")
+    credits6Text.fontFamily = 'Times New Roman';
+    credits6Text.setFontSize(14); 
+    credits6Text.setOrigin(0.5, 0.5); // Center the text
+
+
     
     keyDOWN = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.DOWN);
     
--- src/scenes/Instructions.js
+++ src/scenes/Instructions.js
@@ -11,10 +11,37 @@ create() {
     this.add.image(0, 0, 'instructions').setOrigin(0, 0);
     this.hurtSoundEffect = this.sound.add('hitHurt');
 
-    const instrucText = this.add.text(320, 250, "Move UP")
+    const instrucText = this.add.text(320, 98, "INSTRUCTIONS")
     instrucText.fontFamily = 'Times New Roman';
-    instrucText.setFontSize(20); 
+    instrucText.setFontSize(40); 
     instrucText.setOrigin(0.5, 0.5); // Center the text
+
+    const instruc2Text = this.add.text(270, 150, "Use the UP and DOWN arrow keys to move.")
+    instruc2Text.fontFamily = 'Times New Roman';
+    instruc2Text.setFontSize(20); 
+    instruc2Text.setOrigin(0.5, 0.5); // Center the text
+
+    const instruc3Text = this.add.text(270, 225, "Collect Apples, Cheese and Carrots to \nraise your score.")
+    instruc3Text.fontFamily = 'Times New Roman';
+    instruc3Text.setFontSize(20); 
+    instruc3Text.setOrigin(0.5, 0.5); // Center the text
+
+    const instruc4Text = this.add.text(330, 325, "Avoid Lollipops, Fries and Soda. Colliding with \nthese foods " +
+    "will result in a GAME OVER.")
+    instruc4Text.fontFamily = 'Times New Roman';
+    instruc4Text.setFontSize(20); 
+    instruc4Text.setOrigin(0.5, 0.5); // Center the text
+
+    const instruc5Text = this.add.text(330, 425, "The speed of the game increases every 10 seconds! \n"+
+    "Aim to get the highest score!")
+    instruc5Text.fontFamily = 'Times New Roman';
+    instruc5Text.setFontSize(20); 
+    instruc5Text.setOrigin(0.5, 0.5); // Center the text
+
+    const instruc6Text = this.add.text(515, 470, "Hit DOWN arrow key to go back")
+    instruc6Text.fontFamily = 'Times New Roman';
+    instruc6Text.setFontSize(14); 
+    instruc6Text.setOrigin(0.5, 0.5); // Center the text
     
     keyDOWN = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.DOWN);
     
